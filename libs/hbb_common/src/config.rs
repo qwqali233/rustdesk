0775c1ea00d20cee56bbcfead529f550cb43499c
qwqali233 <143158604+qwqali233@users.noreply.github.com>
2024-11-01 14:18:18 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ali233.xiaobo666.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "q2EZOisHbA5ou2+mJhpEbyFDIkaVRKYa2AvYu2NiT9I=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
